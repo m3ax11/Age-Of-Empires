28e762299c3f6f6d4d92945f6a6bf9c904083417
m3ax11 <eyuphag@gmail.com>
2026-09-15 17:25:25 +0200

    Strengthen political country colours

--- common/defines/zz_AOEIW_graphics.lua
+++ common/defines/zz_AOEIW_graphics.lua
@@ -1,3 +1,5 @@
--- Keep the city-clutter adjustment while inheriting current vanilla lighting,
--- country-colour saturation/brightness and country-border presentation.
+-- Keep vanilla lighting and border presentation, but make political-map
+-- country colours richer and clearer above the terrain layer.
+NDefines.NGraphics.COUNTRY_COLOR_SATURATION_MODIFIER = 0.90
+NDefines.NGraphics.COUNTRY_COLOR_BRIGHTNESS_MODIFIER = 0.88
 NDefines.NGraphics.CITY_SPRAWL_SHRINK_DISTANCE = 360.0
